d960848ac45397ac197f6e3ca349f30bb47907eb
Ashley <panelo.ashley@gmail.com>
2025-06-30 00:07:23 +0800

    Will now bypass the language selection and click the cookie 100 times

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 edge_options = webdriver.EdgeOptions()
 edge_options.add_experimental_option("detach", True)
@@ -9,6 +11,15 @@ driver = webdriver.Edge(options=edge_options)
 
 driver.get("http://ozh.github.io/cookieclicker/")
 
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "langSelect-EN")))
+
 select_en = driver.find_element(By.ID, value="langSelect-EN")
 
 select_en.click()
+
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "bigCookie")))
+
+cookie = driver.find_element(By.ID, value="bigCookie")
+
+for _ in range(100):
+    cookie.click()
