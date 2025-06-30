2d0a8f0a41ec80e2247719e85f61358f4f019b2f
Ashley <panelo.ashley@gmail.com>
2025-06-30 16:02:37 +0800

    Will now click for 5 mins

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+timeout = time.time() + 60*5
 
 edge_options = webdriver.EdgeOptions()
 edge_options.add_experimental_option("detach", True)
@@ -17,9 +20,16 @@ select_en = driver.find_element(By.ID, value="langSelect-EN")
 
 select_en.click()
 
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "bigCookie")))
+# WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "bigCookie")))
+time.sleep(5)
 
 cookie = driver.find_element(By.ID, value="bigCookie")
 
-for _ in range(100):
+while True:
     cookie.click()
+    if time.time() > timeout:
+        break
+
+
+
+
