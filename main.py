f806e2ce916d62d36a496dce759f3047e862871b
Ashley <panelo.ashley@gmail.com>
2025-07-01 16:39:11 +0800

    Will now auto-buy after 500 clicks

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 import selenium.common.exceptions
 
 timeout = time.time() + 60*5
-purchase_time = time.time() + 5
+purchase_time = 0
 
 def buy_upgrades():
     items = 20
@@ -25,7 +24,7 @@ driver = webdriver.Edge(options=edge_options)
 
 driver.get("http://ozh.github.io/cookieclicker/")
 
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "langSelect-EN")))
+WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.ID, "langSelect-EN")))
 
 select_en = driver.find_element(By.ID, value="langSelect-EN")
 
@@ -40,8 +39,10 @@ while True:
     cookie.click()
     if time.time() > timeout:
         break
-    if time.time() > purchase_time:
+    if purchase_time == 500:
         buy_upgrades()
+        purchase_time = 0
+    purchase_time +=1
 
 
 
